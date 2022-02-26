1cf2948cfed41e228271aee2ebb54c99c35c23c7
Mundurujoana <mundujoana@gmail.com>
2022-02-26 21:21:11 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    var campus = "akirachix"
+    var char1 = campus[0]
+    var char2 = campus[2]
+    var char3 = campus[3]
+    var result = campus[0].toString() + campus[2] + campus[3]
+    println(result)
+
+
+    var fullName = "joana"
+    var age = 16
+    println("Hi, my name is $fullName and I am $age years old.")
+
+
+    val hoMwk = "assignment"
+    println(hoMwk.length)
+
+    var myName = "sarah"
+    if (myName == "mike") {
+        println("That's me!")
+    } else{
+        println("I don’t know who that is")
+    }
+    var fName = "sarah"
+    if (myName == "sarah") {
+        println("That's me!")
+    } else{
+        println("I don’t know who that is")
+    }
+}
