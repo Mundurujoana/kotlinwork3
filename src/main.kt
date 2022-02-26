3964c972105512323b4c0874b4a0d53c21b21288
Munduru joana <99388600+Mundurujoana@users.noreply.github.com>
2022-02-26 22:18:52 +0300

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,20 +1,25 @@
 fun main() {
+    //question1
+    
     var campus = "akirachix"
     var char1 = campus[0]
     var char2 = campus[2]
     var char3 = campus[3]
     var result = campus[0].toString() + campus[2] + campus[3]
     println(result)
-
+    
+    //question2
 
     var fullName = "joana"
     var age = 16
     println("Hi, my name is $fullName and I am $age years old.")
 
-
+    //question3
+    
     val hoMwk = "assignment"
-    println(hoMwk.length)
+    println("The lenght of the string is: " + hoMwk.length)
 
+    //question4
     var myName = "sarah"
     if (myName == "mike") {
         println("That's me!")
