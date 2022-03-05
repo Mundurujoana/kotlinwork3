d486eb4ad0063173f6a27bf18a9d687d48a8d506
Munduru joana <99388600+Mundurujoana@users.noreply.github.com>
2022-03-05 07:56:15 +0300

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,36 +1,51 @@
 fun main() {
-    //question1
-    
-    var campus = "akirachix"
-    var char1 = campus[0]
-    var char2 = campus[2]
-    var char3 = campus[3]
-    var result = campus[0].toString() + campus[2] + campus[3]
-    println(result)
-    
+   //question 1
+    school()
+
     //question2
+    myNames()
 
-    var fullName = "joana"
-    var age = 16
-    println("Hi, my name is $fullName and I am $age years old.")
-
-    //question3
-    
-    val hoMwk = "assignment"
-    println("The lenght of the string is: " + hoMwk.length)
+    //question 3
+   noOfchar("assignmemt")
 
     //question4
-    var myName = "sarah"
-    if (myName == "mike") {
-        println("That's me!")
-    } else{
-        println("I don’t know who that is")
-    }
-    
-    var fName = "sarah"
-    if (myName == "sarah") {
-        println("That's me!")
-    } else{
-        println("I don’t know who that is")
-    }
+    myIdentity("sarah")
+    myIdentity("joana")
+
+
 }
+
+
+//question1
+fun school(){
+var campus = "akirachix"
+var char1 = campus[0]
+var char2 = campus[2]
+var char3 = campus[3]
+var result = campus[0].toString() + campus[2] + campus[3]
+println(result)
+}
+
+
+//question2
+fun myNames(){
+var fullName = "joana"
+var age = 16
+println("Hi, my name is $fullName and I am $age years old.")
+}
+
+//question3
+fun noOfchar(x:String):Int{
+return x.length
+}
+
+fun myIdentity(name:String){
+    if (name == "sarah") {
+        println("That's me!")
+    } else{
+        println("I don’t know who that is")
+    }
+
+}
+   
+
